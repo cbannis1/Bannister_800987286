2926e9e30f0d702a8c50ccf4972ff66c9e8e44d9
cbannis1 <46727270+cbannis1@users.noreply.github.com>
2020-06-08 17:37:41 -0400

    added background stuff

--- blog.js
+++ blog.js
@@ -1,3 +1,7 @@
-function background(){
-    document.body.style.background = "url('Mountain home.jpg') no repeat";
+function background1() {
+    document.getElementById("myBody").style.background = "url('simple.jpg') no-repeat";
 }
+function background2() {
+    document.getElementById("myBody").style.background = "url('gradient.png') no-repeat";
+}
+
