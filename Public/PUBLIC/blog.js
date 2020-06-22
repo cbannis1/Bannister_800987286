71d5acb4e29a95d9f2e56d2913185355d78218e2
cbannis1 <46727270+cbannis1@users.noreply.github.com>
2020-06-22 12:23:32 -0400

    preparing for merge

--- Public/PUBLIC/blog.js
+++ Public/PUBLIC/blog.js
@@ -12,28 +12,6 @@ function smaller(){
     document.getElementById("myBody").style.fontSize = "1.0em";
 }
 
-/**$(document).ready(function(){
-
-    $('form').on('submit', function(){
-  
-        var item = $('form input');
-        var post = {id: id.val()};
-  
-        $.ajax({
-          type: 'POST',
-          url: '/views/post',
-          data: post,
-          success: function(post){
-            location.reload();
-          }
-        });
-  
-        return false;
-  
-    });
-  });**/
-
-  function addPost(){
-    console.log(document.forms[0].Content.value);
+function addPost(){
     console.log("hello world");
-  }
+}
