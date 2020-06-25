780c27c3f3a6082680cd65439f5d4192cfc0ec40
cbannis1 <46727270+cbannis1@users.noreply.github.com>
2020-06-25 14:09:50 -0400

    milestone 5 work added upload file function

--- Milestone-5/routes/posts.js
+++ Milestone-5/routes/posts.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const Post = require('./../models/post')
 const router = express.Router()
+//for reading the file 
+var fs = require('fs')
 
 router.get('/add_post', (req, res) =>{
     res.render('admin/add_post', {post: new Post() })
@@ -16,35 +18,53 @@ router.get('/:id', async (req, res) =>{
     res.render('admin/list_posts', { post: post})
 })
 
+//handles the post request from add_post
+//async and await are used so the post can be added before it is reloaded 
 router.post('/', async (req, res ) => {
         let post = new Post({
         title: req.body.title,
         content: req.body.content
         })
-        try {
-          post = await post.save()
-          res.redirect(`/admin/${post.id}`)
-        } catch (error) {
-          res.render('admin/add_post', { post: post })
+        //if there is a file being passed as a parameter
+        if (req.files){
+          var file = req.files.filename,//declared variable file to be used for the filename 
+          filename=file.name//assign the filename to be the file with proprety name 
+          //create variable content and assign it to this function which takes in the content and then assigns post.content to the content recieved 
+          fs.readFile(filename, 'utf8', function (err, filecontent){//reads the file, txt
+            req.params.content = filecontent
+          }) 
         }
+        //if there is no file proceed to add the post as normal 
+          else {
+            try {
+              post = await post.save()//needs await here because of the async above 
+              res.redirect(`/admin/${post.id}`)//this will send the user back to the admin route using the specified post.id 
+            } catch (error) {
+              res.render('admin/add_post')//if this doesnt work it should return the user back to the updat_post page 
+            }
+          }
+        
     })
 
-  
-router.put('/:id',  async (req, res ) => {
+//handles the put request for update_post  
+//still getting unhandled promise rejections when trying to visit this page 
+router.put('/:id',  async (req, res ) => {//does not work without async, reloads the page before it is able to update the post 
     let post = new Post({
     title: req.body.title,
     content: req.body.content
     })
     try {
-      post =  await post.save()
-      res.redirect(`/admin/${post.id}`)
+      post =  await post.save()//needs await here because of the async above 
+      res.redirect(`/admin/${post.id}`)//this will send the user back to the admin route using the specified post.id 
     } catch (error) {
-      res.render('admin/update_post', { post: post })
+      res.render('admin/update_post')//if this doesnt work it should return the user back to the updat_post page 
     }
 })
 
-router.delete('/:id', async (req, res) => {
-    await Post.findByIdAndDelete(req.params.id)
+//handles the delete request 
+//asynchronous because of unhandled promise rejections error solved by using async and then await
+router.delete('/:id', async(req, res) => { //does not work without async, it seems to reload the page before deleting 
+     await Post.findByIdAndDelete(req.params.id)
     res.redirect('/')
 })
 
--- Milestone-5/server.js
+++ Milestone-5/server.js
@@ -4,6 +4,9 @@ const Post = require('./models/post')
 const postsRouter = require('./routes/posts')
 const methodOverride = require('method-override')
 const app = express()
+//used to upload files 
+upload = require("express-fileupload")
+app.use(upload())
 
 app.use(express.static('Assets'))
 
@@ -14,7 +17,7 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
 
-app.get('/', async (req,res) =>{
+app.get('/', async (req,res) =>{//async await resolved "posts.forEach is not a function" error in this line 
     const posts = await Post.find()
     res.render('index' , { posts: posts})
 })
