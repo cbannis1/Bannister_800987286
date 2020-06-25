7d5670ce2c31a3bf67c454d19797253f799560ab
cbannis1 <46727270+cbannis1@users.noreply.github.com>
2020-06-25 18:38:27 -0400

    added files for rest api

--- Milestone-5/server.js
+++ Milestone-5/server.js
@@ -2,9 +2,11 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Post = require('./models/post')
 const postsRouter = require('./routes/posts')
+const usersRouter = require('./routes/users')
 const methodOverride = require('method-override')
+const bodyParser = require('body-parser')
 const app = express()
-
+app.use(bodyParser.json())
 app.use(express.static('Assets'))
 
 mongoose.connect('mongodb://localhost/blog', {
@@ -19,6 +21,7 @@ app.get('/', async (req,res) =>{
     res.render('index' , { posts: posts})
 })
 
+app.use('/',usersRouter)
 app.use('/admin', postsRouter)
 
 app.listen(4000)
